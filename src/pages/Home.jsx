a61677101430db12c25f65df15f72aa28216752d
Sabbir Hasan <xofficial490@gmail.com>
2025-06-30 09:38:02 +0600

    new change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroCarosoul from '../components/Home/carosol/HeroCarosoul';
-import Features from '../components/features';
+import Features from '../components/Features'
 import Product from '../components/products/Product';
 import DiscountSection from '../../src/components/Home/discount/DiscountSection'
 import Category from '../components/Home/category/Category';
